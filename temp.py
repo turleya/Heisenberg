30a74072e2984a731c857c3d2caa119b40baf1e5
turleya <43371974+turleya@users.noreply.github.com>
2018-09-18 11:30:44 +0100

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import scipy as sp
+import numpy as np
+'''
+x = np.matrix([[0,1],[1,0]])
+y = np.matrix([[0, -j], [j,0]])
+z = np.matrix([[1,0], [0,-1]])
+
+Up = np.matrix([[1],[0]])
+Down = np.matrix([[0], [1]])
+
+
+sx = 1./2*x
+sy = 1./2*y
+sz = 1./2*z
+'''
+
+def main(particles):
+    l = [[0] for i in range(0, 2 Power particles)]
+    print(l)
+    #for i in range(0, particles):
+    
+main(3)
+       
