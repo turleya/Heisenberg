ef041d4d896043fcf34b19bf67f4aad6bdbe3db8
turleya <43371974+turleya@users.noreply.github.com>
2018-10-08 17:24:58 +0100

    Add files via upload

--- Heisenberg2.py
+++ Heisenberg2.py
@@ -43,9 +43,7 @@ def spinoperatorz(particles,index):
             P_i = np.bmat([[sz[0,0]*P_i, sz[0,1]*P_i], [sz[1,0]*P_i, sz[1,1]*P_i]]) 
         else:
             P_i = np.bmat([[I[0,0]*P_i, I[0,1]*P_i], [I[1,0]*P_i, I[1,1]*P_i]]) 
-    return(P_i)
-
-spinoperatorz(3,1) 
+    return(P_i) 
   
 #Splus operator
 def spinoperatorplus(particles,index): 
@@ -58,9 +56,7 @@ def spinoperatorplus(particles,index):
             P_i = np.bmat([[splus[0,0]*P_i, splus[0,1]*P_i], [splus[1,0]*P_i, splus[1,1]*P_i]]) 
         else:
             P_i = np.bmat([[I[0,0]*P_i, I[0,1]*P_i], [I[1,0]*P_i, I[1,1]*P_i]]) 
-    return(P_i)
-
-spinoperatorplus(3,3) 
+    return(P_i) 
 
 #Sminus operator
 def spinoperatorminus(particles,index): 
@@ -73,29 +69,30 @@ def spinoperatorminus(particles,index):
             P_i = np.bmat([[sminus[0,0]*P_i, sminus[0,1]*P_i], [sminus[1,0]*P_i, sminus[1,1]*P_i]]) 
         else:
             P_i = np.bmat([[I[0,0]*P_i, I[0,1]*P_i], [I[1,0]*P_i, I[1,1]*P_i]]) 
-    return(P_i)
-
-spinoperatorminus(3,3) 
+    return(P_i) 
 
 
-J_ij = 1.0
-H_i = 0.5*J_ij*(spinoperatorplus(3,1)*spinoperatorminus(3,2) + spinoperatorplus(3,2)*spinoperatorminus(3,1)) + J_ij*(spinoperatorz(3,1)*spinoperatorz(3,2)) + 0.5*J_ij*(spinoperatorplus(3,3)*spinoperatorminus(3,2) + spinoperatorplus(3,2)*spinoperatorminus(3,3)) + J_ij*(spinoperatorz(3,3)*spinoperatorz(3,2))+ 0.5*J_ij*(spinoperatorplus(3,1)*spinoperatorminus(3,3) + spinoperatorplus(3,3)*spinoperatorminus(3,1)) + J_ij*(spinoperatorz(3,1)*spinoperatorz(3,3))
-print(H_i)
-
-w, v = la.eig(H_i)
-print w
-print v
-
-"""
 #Heisenberg Hamiltonian 
 def Hamiltonian(particles):
-    H = np.zeros([2**particles,2**particles])
     J_ij = 1.0 #anistropy parameter
-    B_i = 0.0 #local magnetic field
-    for i in range(particles-1):	
-	H_i = 0.5*J_ij*(spinoperatorplus(particles,i+1)*spinoperatorminus(particles,i+2) + spinoperatorplus(particles,i+2)*spinoperatorminus(particles,i+1)) + J_ij*(spinoperatorz(particles,i+1)*spinoperatorz(particles,i+2)) + B_i*spinoperatorz(particles, i+1) 
+    B_j = 0.1 #local magnetic field
+
+    #Chain
+    #H = np.zeros([2**particles,2**particles])
+
+    #Loop
+    H = 0.5*J_ij*(spinoperatorplus(particles,1)*spinoperatorminus(particles,particles) + spinoperatorplus(particles,particles)*spinoperatorminus(particles,1)) + J_ij*(spinoperatorz(particles,1)*spinoperatorz(particles,particles))
+
+    #Particles not interacting
+    #H = -(0.5*J_ij*(spinoperatorplus(particles,2)*spinoperatorminus(particles,3) + spinoperatorplus(particles,3)*spinoperatorminus(particles,2)) + J_ij*(spinoperatorz(particles,2)*spinoperatorz(particles,3)))
+
+    for i in range(particles-1):
+	H_i = 0.5*J_ij*(spinoperatorplus(particles,i+1)*spinoperatorminus(particles,i+2) + spinoperatorplus(particles,i+2)*spinoperatorminus(particles,i+1)) + J_ij*(spinoperatorz(particles,i+1)*spinoperatorz(particles,i+2))
 	H = H + H_i
-    print(H)
+    for j in range(particles):	
+	H_j = B_j*spinoperatorz(particles, j+1)
+	H = H + H_j
+    return(H)
 
 Ham = Hamiltonian(3)
 
@@ -105,19 +102,38 @@ def eigen(particles):
     for i in range(0, 2**particles):
 	w_i = w[i] 
 	v_i = v[i]
-	print(round(w_i,2)) #eigenvalues
+	print(round(w_i,4)) #eigenvalues
 	print(np.around(v_i, decimals=2)) #eigenvectors
 
 eigen(3)   
 
+#Computing expectation values
+def expecval(particles):
+    for i in range(particles):
+	Ket = spinoperatorz(particles, i+1)*v_i
+    return Ket
 
- + 0.5*J_ij*(spinoperatorplus(particles,1)*spinoperatorminus(particles,particles) + spinoperatorplus(particles,particles)*spinoperatorminus(particles,1)) + J_ij*(spinoperatorz(particles,1)*spinoperatorz(particles,particles))
+expecval(3)
 
-+ 0.5*J_ij*(spinoperatorplus(3,1)*spinoperatorminus(3,3) + spinoperatorplus(3,3)*spinoperatorminus(3,1)) + J_ij*(spinoperatorz(3,1)*spinoperatorz(3,3))
 """
 
+#Test
+J_ij = 1.0
+B_i = 0.1
+H = 0.5*J_ij*(spinoperatorplus(3,1)*spinoperatorminus(3,2) + spinoperatorplus(3,2)*spinoperatorminus(3,1)) + J_ij*(spinoperatorz(3,1)*spinoperatorz(3,2)) + 0.5*J_ij*(spinoperatorplus(3,2)*spinoperatorminus(3,3) + spinoperatorplus(3,3)*spinoperatorminus(3,2)) + J_ij*(spinoperatorz(3,2)*spinoperatorz(3,3)) + B_i*(spinoperatorz(3,1) + spinoperatorz(3,2) + spinoperatorz(3,3))
 
 
+
+def eigen(particles):
+    w, v = la.eig(H)
+    for i in range(0, 2**particles):
+	w_i = w[i] 
+	v_i = v[i]
+	print(round(w_i,4)) #eigenvalues
+	print(np.around(v_i, decimals=2)) #eigenvectors
+
+eigen(3) 
+"""
 
 
 
