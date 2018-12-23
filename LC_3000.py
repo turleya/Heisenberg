f9e06f43c7da64cc01fbfb9167253b0d8853e45f
turleya <43371974+turleya@users.noreply.github.com>
2018-12-23 14:16:37 +0000

    Add files via upload

--- LC_3000.py
+++ LC_3000.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
+import matplotlib
 import imp
 import pandas as pd
 import seaborn as sns #advanced graphing library
@@ -17,28 +18,33 @@ seed = 1010
 np.random.seed(seed)
 random.seed(seed)
 
-Heis_data =np.load('sl_30000_data.npz') #each row contains a feature vector
+Heis_data =np.load('sl_100000_data.npz') #each row contains a feature vector
 designmatrix = Heis_data['designmatrix']
 Energies_1 = Heis_data['GS_Energies'] #Target vectors
 
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 12}
+matplotlib.rc('font', **font)
+
 #Ground State Energies
-Energies = np.zeros(30000)
-for i in range(30000):
+Energies = np.zeros(100000)
+for i in range(100000):
     Energies[i] = Energies_1[i,0]
 print(Energies)
 
-"""
+
 model = Sequential() #defining model for Heisenberg Hamiltonian
 model.add(Dense(input_dim=20, units=256, activation='relu'))
 model.add(Dense(units=256, activation='relu'))
 model.add(Dense(units=256, activation='relu'))
 model.add(Dense(units=1))
-"""
+
 
 MSE_val = []
 MSE_train = []
 dm = []
-for i in range(500, 30000, 1000):
+for i in range(500, 100000, 5000):
     designmatrix_tv = []
     designmatrix_test = []
     Energies_tv = []
@@ -68,17 +74,12 @@ for i in range(500, 30000, 1000):
     print(model.summary())
 
     callback_list = [EarlyStopping(monitor='val_loss', min_delta=1e-7, patience=10, verbose=1)]
-    n_epochs = 100
+    n_epochs = 150
 
     #Model fit
     model_history = model.fit(x=designmatrix_train, y=Energies_train_std, validation_data=(designmatrix_val, Energies_val_std), 
                 batch_size=32, verbose=1, 
                 epochs=n_epochs, callbacks=callback_list, shuffle=True)
-
-    #Generalization Error
-    #Predictions
-    #Energies_pred_train = model_best.predict(designmatrix_train)*Energies_std + Energies_mu
-    #Energies_pred_val = model_best.predict(designmatrix_val)*Energies_std + Energies_mu
 
     indx = len(model_history.history['val_loss'])
     val_loss = (model_history.history['val_loss'])[indx-1]
@@ -95,8 +96,8 @@ print(MSE_val)
 
 #Learning Curve
 plt.figure()
-plt.plot(dm, MSE_train, label='Train')
-plt.plot(dm, MSE_val, label='Val')
+plt.plot(dm, MSE_train, label='Training set MSE')
+plt.plot(dm, MSE_val, label='Validation set MSE')
 plt.legend()
 plt.xlabel('Number of Samples in Training set')
 plt.ylabel('Mean Squared Error')
